614332a07901b2c10c74fc4bdf5635a9c93f7801
Agnish Upadhyay <agnish@iitg.ac.in>
2023-06-30 16:13:56 +0530

    xyz

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -72,7 +72,7 @@
         .article-image {
             width: 100%;
             height: auto;
-            margin-bottom: 20px;
+            margin-bottom: 21px;
             border-radius: 5px;
             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
         }
